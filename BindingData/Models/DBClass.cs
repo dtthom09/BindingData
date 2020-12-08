97d7ef68196306617006bf43f393592165f8906a
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2020-12-08 18:31:42 -0500

    connection vs to ssms

---
+++ BindingData/Models/DBClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BindingData.Models
+{
+    public class DBClass : DbContext
+    {
+        public DBClass(DbContextOptions<DBClass> options) : base(options)
+        {
+
+        }
+        public DbSet<EmpClass> Students1 { get; set; }
+    }
+}
---
+++ BindingData/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BindingData.Models;
+
+namespace BindingData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly ILogger<IndexModel> _logger;
+        private readonly DBClass _db;
+
+        public IndexModel(DBClass db)//ILogger<IndexModel> logger, DBClass db)
+        {
+            //_logger = logger;
+            _db = db;
+        }
+
+        public IEnumerable<EmpClass> Getrecords { get; set; }
+
+        public async Task OnGet()
+        {
+            List<EmpClass> lists = await _db.Students1.ToListAsync();
+            Getrecords = lists;
+        }
+    }
+}
